ab0e484c09970087030ac92cd358a179414002d7
sohilraza01 <rsohil204@gmail.com>
2025-12-04 19:36:49 +0530

    admin-dashboard-completed

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,29 @@ import Footer from './components/Footer';
 import AllRooms from './Pages/AllRooms';
 import RoomDetails from './Pages/RoomDetails';
 import MyBookings from './Pages/MyBookings';
+import HotelReg from './components/HotelReg';
+import Layout from './Pages/hotelOwner/Layout';
+import Dashboard from './Pages/hotelOwner/Dashboard';
+import AddRoom from './Pages/hotelOwner/AddRoom';
+import ListRoom from './Pages/hotelOwner/ListRoom';
 
 const App = () => {
   const isOwner = useLocation().pathname.includes("owner");
   return (
     <div>
      {!isOwner && <Navbar />}
+     {false && <HotelReg />}
      <div className='min-h-[70vh]'>
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='/rooms' element={<AllRooms/>} />
         <Route path='/rooms/:id' element={<RoomDetails/>} />
         <Route path='/my-bookings' element={<MyBookings/>} />
+        <Route path='/owner' element={<Layout/>}>
+        <Route index element={<Dashboard />} />
+        <Route path='add-room' element={<AddRoom />} />
+        <Route path='list-room' element={<ListRoom />} />
+        </Route>
 
       </Routes>
      </div>
