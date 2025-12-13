399f20a6bae1111b14228a2bcc1e6cc70475a527
sohilraza01 <rsohil204@gmail.com>
2025-12-13 13:28:17 +0530

    db-connected

--- server/server.js
+++ server/server.js
@@ -1,10 +1,23 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import connectDB from './configs/db.js';
+import { clerkMiddleware } from '@clerk/express'
+import clerkWebhooks from './controllers/clerkWebhooks.js';
+
+connectDB();
 
 const app = express();
 
 app.use(cors());
+
+app.use(express.json());
+app.use(clerkMiddleware());
+
+
+// API to listen to clerk webhooks
+app.use("/api/clerk",clerkWebhooks);
+
 app.get("/",(req,res)=>res.send('Hello Server!'));
 
 const PORT  = process.env.PORT || 3000;
